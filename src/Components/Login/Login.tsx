63abe5f2e7b540c3c72c13326be8bff669186199
oyr230125 <oyr230125@omsecurity.kr>
2023-09-27 14:50:38 +0900

    로그인 파일 다운로드 시 레이아웃 움직이는 현상 수정

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -227,7 +227,7 @@ const Login = () => {
             {windowWidth <= 785 ?
             <></>
             :
-            <div>
+            <div className='login_agent_download_button_container'>
               {isAgentFileDisable ?
               <Spin className='login_agent_download_loading' indicator={antIcon} />
               :
@@ -237,29 +237,6 @@ const Login = () => {
                 onClick={() => {
                   if(!isAgentFileDisable) {
                     AgentFileDownload(setIsAgentFileDisable, formatMessage({ id: 'DOWNLOAD_FAILED' }));
-                    // setIsAgentFileDisable(true);
-                    // CustomAxiosGetFile(
-                    //   GetAgentInstallerDownloadApi,
-                    //   (res:any) => {
-                    //     const versionName = res.headers['content-disposition'].split(';').filter((str:any) => str.includes('filename'))[0].match(/filename="([^"]+)"/)[1];
-                    //     const fileDownlaoadUrl = URL.createObjectURL(res.data);
-                    //     const downloadLink = document.createElement('a');
-                    //     downloadLink.href = fileDownlaoadUrl;
-                    //     downloadLink.download = versionName;
-                    //     document.body.appendChild(downloadLink);
-                    //     downloadLink.click();
-                    //     document.body.removeChild(downloadLink);
-                    //     URL.revokeObjectURL(fileDownlaoadUrl);
-                    //   },
-                    //   {
-                    //   },
-                    //   () => {
-                    //     message.error(formatMessage({ id: 'DOWNLOAD_FAILED' }));
-                    //   }, {},
-                    //   () => {
-                    //     setIsAgentFileDisable(false);
-                    //   }
-                    // )  
                   }
                 }}
               >
@@ -269,9 +246,6 @@ const Login = () => {
               }
             </div>
             }
-
-
-
             {/* macOS 추가 */}
             {/* <div>
               <button
